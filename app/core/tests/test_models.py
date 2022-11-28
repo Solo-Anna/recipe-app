f74cc4a36c18f431f20f55d72d18f7a74e5f56f3
Solo-Anna <108534103+Solo-Anna@users.noreply.github.com>
2022-11-28 23:40:01 +0300

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,5 @@
 """
-Tests for models
+Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -9,22 +9,23 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successfull(self):
-        "Test creating a user with an email is successful."
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email=email, password=password,
-            )
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    def test_new_email_normalized(self):
+    def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
@@ -37,9 +38,10 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
-        """Test creating a superuser"""
+        """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
-                'test@example.com', 'test123',
-                )
+                'test@example.com',
+                'test123',
+        )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
