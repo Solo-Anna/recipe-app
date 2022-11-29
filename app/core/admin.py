cdeb7b422be46c2d9240de07c2d656af457959ab
Solo-Anna <108534103+Solo-Anna@users.noreply.github.com>
2022-11-29 23:59:51 +0300

    tests update

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (_('Main'), {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
             {
@@ -28,7 +28,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
-
+    add_fieldsets = (
+        (None, {
+        'classes' :('wide',),
+        'fields': (
+            'email',
+            'password1',
+            'password2',
+            'name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+        )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,7 +12,7 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 
 
-def reate_user(**params):
+def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
@@ -53,13 +53,14 @@ class PublicUserAPiTEsts(TestCase):
         """Test an error is returned if password less than 5 chars. """
         payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
+            'password': 'pw',
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email = payload['email']).exists()
+            email=payload['email']
+        ).exists()
         self.assertFalse(user_exists)
 
