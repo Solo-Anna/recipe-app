84d62ad12831a81d513da5ce3238801a7486c3da
Solo-Anna <108534103+Solo-Anna@users.noreply.github.com>
2022-12-06 12:08:08 +0300

    recipe with tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,4 +1,6 @@
-"""Tests for the tags API."""
+"""
+Tests for the tags API.
+"""
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -15,7 +17,7 @@ TAGS_URL = reverse('recipe:tag-list')
 
 def detail_url(tag_id):
     """Create and return a tag detail url."""
-    return reverse('reverse:tag-detail', args=[tag_id])
+    return reverse('recipe:tag-detail', args=[tag_id])
 
 
 def create_user(email='user@example.com', password='testpass123'):
@@ -40,8 +42,8 @@ class PrivateTagsApiTests(TestCase):
     """Test authenticated API requests"""
 
     def setUp(self):
-        self.client = APIClient()
         self.user = create_user()
+        self.client = APIClient()
         self.client.force_authenticate(self.user)
 
     def test_retrieve_tags(self):
@@ -59,8 +61,8 @@ class PrivateTagsApiTests(TestCase):
 
     def test_tags_limited_to_user(self):
         """Test list of tags is limited to authenticated user."""
-        user2=create_user(email='user2@example.com')
-        Tag.objects.create(user=user2, name='Frutty')
+        user2 = create_user(email='user2@example.com')
+        Tag.objects.create(user=user2, name='Fruity')
         tag = Tag.objects.create(user=self.user, name='Comfort Food')
 
         res = self.client.get(TAGS_URL)
@@ -69,7 +71,6 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
-
 
     def test_update_tag(self):
         """Test updating a tag."""
@@ -82,6 +83,17 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
 
 
 
